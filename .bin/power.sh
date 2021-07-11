04d4379f07df27de9a0fdc74e55d1b4b81c2073b
Miguel Rodrigues <miguelangelorodrigues@enta.pt>
2021-07-11 17:58:22 +0000

    up

--- .bin/power.sh
+++ .bin/power.sh
@@ -11,7 +11,7 @@
 # - Give it exec priviledge, or chmod +x /path/to/power.sh
 # - Run it
 
-chosen=$(echo -e "[Cancel]\nLogout\nRestart WM\nShutdown\nReboot\nSuspend\nHibernate\nHybrid-sleep\nSuspend-then-hibernate" | rofi -dmenu -i)
+chosen=$(echo -e "[Cancel]\nLogout\nRestart WM\nShutdown\nReboot\nSuspend\nHibernate\nSuspend-then-hibernate" | rofi -dmenu -i)
 # Info about some states are available here:
 # https://www.freedesktop.org/software/systemd/man/systemd-sleep.conf.html#Description
 
@@ -26,8 +26,6 @@ elif [[ $chosen = "Reboot" ]]; then
 elif [[ $chosen = "Suspend" ]]; then
 	systemctl suspend
 elif [[ $chosen = "Hibernate" ]]; then
-	systemctl hibernate
-elif [[ $chosen = "Hybrid-sleep" ]]; then
 	systemctl hibernate
 elif [[ $chosen = "Suspend-then-hibernate" ]]; then
 	systemctl suspend-then-hibernate
